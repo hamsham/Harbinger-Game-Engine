d8de898c537db2be493852aeb6c40be3e5294ebe
hammy <hammy@hambox-9000>
2013-06-08 15:28:14 -0700

    Updated Linux build

--- harbinger-netbeans/nbproject/Makefile-variables.mk
+++ harbinger-netbeans/nbproject/Makefile-variables.mk
@@ -7,37 +7,37 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug_DLL configuration
-CND_PLATFORM_Debug_DLL=MinGW_TDM-Windows
+CND_PLATFORM_Debug_DLL=GNU-Linux-x86
 CND_ARTIFACT_DIR_Debug_DLL=../bin/WIN32
 CND_ARTIFACT_NAME_Debug_DLL=harbinger-debug.dll
 CND_ARTIFACT_PATH_Debug_DLL=../bin/WIN32/harbinger-debug.dll
-CND_PACKAGE_DIR_Debug_DLL=dist/Debug_DLL/MinGW_TDM-Windows/package
-CND_PACKAGE_NAME_Debug_DLL=libharbinger-netbeans.dll.tar
-CND_PACKAGE_PATH_Debug_DLL=dist/Debug_DLL/MinGW_TDM-Windows/package/libharbinger-netbeans.dll.tar
+CND_PACKAGE_DIR_Debug_DLL=dist/Debug_DLL/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug_DLL=libharbinger-netbeans.so.tar
+CND_PACKAGE_PATH_Debug_DLL=dist/Debug_DLL/GNU-Linux-x86/package/libharbinger-netbeans.so.tar
 # Release_DLL configuration
-CND_PLATFORM_Release_DLL=MinGW_TDM-Windows
+CND_PLATFORM_Release_DLL=GNU-Linux-x86
 CND_ARTIFACT_DIR_Release_DLL=../bin/WIN32
 CND_ARTIFACT_NAME_Release_DLL=harbinger.dll
 CND_ARTIFACT_PATH_Release_DLL=../bin/WIN32/harbinger.dll
-CND_PACKAGE_DIR_Release_DLL=dist/Release_DLL/MinGW_TDM-Windows/package
-CND_PACKAGE_NAME_Release_DLL=libharbinger-netbeans.dll.tar
-CND_PACKAGE_PATH_Release_DLL=dist/Release_DLL/MinGW_TDM-Windows/package/libharbinger-netbeans.dll.tar
+CND_PACKAGE_DIR_Release_DLL=dist/Release_DLL/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release_DLL=libharbinger-netbeans.so.tar
+CND_PACKAGE_PATH_Release_DLL=dist/Release_DLL/GNU-Linux-x86/package/libharbinger-netbeans.so.tar
 # Debug__Linux_ configuration
-CND_PLATFORM_Debug__Linux_=MinGW_TDM-Windows
+CND_PLATFORM_Debug__Linux_=GNU-Linux-x86
 CND_ARTIFACT_DIR_Debug__Linux_=../bin/LINUX
 CND_ARTIFACT_NAME_Debug__Linux_=libharbinger-debug.so
 CND_ARTIFACT_PATH_Debug__Linux_=../bin/LINUX/libharbinger-debug.so
-CND_PACKAGE_DIR_Debug__Linux_=dist/Debug__Linux_/MinGW_TDM-Windows/package
-CND_PACKAGE_NAME_Debug__Linux_=libharbinger-netbeans.dll.tar
-CND_PACKAGE_PATH_Debug__Linux_=dist/Debug__Linux_/MinGW_TDM-Windows/package/libharbinger-netbeans.dll.tar
+CND_PACKAGE_DIR_Debug__Linux_=dist/Debug__Linux_/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug__Linux_=libharbinger-netbeans.so.tar
+CND_PACKAGE_PATH_Debug__Linux_=dist/Debug__Linux_/GNU-Linux-x86/package/libharbinger-netbeans.so.tar
 # Release__Linux_ configuration
-CND_PLATFORM_Release__Linux_=MinGW_TDM-Windows
+CND_PLATFORM_Release__Linux_=GNU-Linux-x86
 CND_ARTIFACT_DIR_Release__Linux_=../bin/LINUX
 CND_ARTIFACT_NAME_Release__Linux_=libharbinger.so
 CND_ARTIFACT_PATH_Release__Linux_=../bin/LINUX/libharbinger.so
-CND_PACKAGE_DIR_Release__Linux_=dist/Release__Linux_/MinGW_TDM-Windows/package
-CND_PACKAGE_NAME_Release__Linux_=libharbinger-netbeans.dll.tar
-CND_PACKAGE_PATH_Release__Linux_=dist/Release__Linux_/MinGW_TDM-Windows/package/libharbinger-netbeans.dll.tar
+CND_PACKAGE_DIR_Release__Linux_=dist/Release__Linux_/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release__Linux_=libharbinger-netbeans.so.tar
+CND_PACKAGE_PATH_Release__Linux_=dist/Release__Linux_/GNU-Linux-x86/package/libharbinger-netbeans.so.tar
 #
 # include compiler specific variables
 #
--- harbinger-netbeans/nbproject/Package-Debug__Linux_.bash
+++ harbinger-netbeans/nbproject/Package-Debug__Linux_.bash
@@ -6,7 +6,7 @@
 
 # Macros
 TOP=`pwd`
-CND_PLATFORM=MinGW_TDM-Windows
+CND_PLATFORM=GNU-Linux-x86
 CND_CONF=Debug__Linux_
 CND_DISTDIR=dist
 CND_BUILDDIR=build
@@ -14,7 +14,7 @@ NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
 TMPDIRNAME=tmp-packaging
 OUTPUT_PATH=../bin/LINUX/libharbinger-debug.so
 OUTPUT_BASENAME=libharbinger-debug.so
-PACKAGE_TOP_DIR=libharbinger-netbeans.dll/
+PACKAGE_TOP_DIR=libharbinger-netbeans.so/
 
 # Functions
 function checkReturnCode
@@ -59,15 +59,15 @@ mkdir -p ${NBTMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory "${NBTMPDIR}/libharbinger-netbeans.dll/lib"
+makeDirectory "${NBTMPDIR}/libharbinger-netbeans.so/lib"
 copyFileToTmpDir "${OUTPUT_PATH}" "${NBTMPDIR}/${PACKAGE_TOP_DIR}lib/${OUTPUT_BASENAME}" 0644
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/libharbinger-netbeans.dll.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/libharbinger-netbeans.so.tar
 cd ${NBTMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/libharbinger-netbeans.dll.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/libharbinger-netbeans.so.tar *
 checkReturnCode
 
 # Cleanup
